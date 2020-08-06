685f4cc0e79a1512f542b689f224ff5950f669c0
Asiya59 <46769457+Asiya59@users.noreply.github.com>
2020-08-05 21:33:04 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -4,6 +4,9 @@ public class GitFun {
     public static void main(String[] args) {
 
         System.out.println("Everything is fun");
+        
+        
+        //this line is edit from Github
     }
 
 }
